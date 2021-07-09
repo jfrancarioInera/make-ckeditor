84c82347e44a1ca04b9d7420cdb45ccdf528317f
James <j.francario@inera.it>
2021-07-09 11:17:28 +0200

    fixed problem, couldn't edit text

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,7 +7,7 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
@@ -19,30 +19,30 @@ export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
-	Essentials,
+  Essentials,
+  Heading,
+  Bold,
+  Italic,
+  Underline,
 
-	Bold,
-	Italic,
-	Underline,
-
-	Link,
-	List,
+  Link,
+  List,
 ];
-ClassicEditor.defaultConfig = {
-	toolbar: {
-		items: [
-			'bold',
-			'italic',
-			'underline',
-			'|',
-			'bulletedList',
-			'numberedList',
-			'|',
-			'link',
-		],
-	},
-	// Editor configuration.
 
-	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en',
+// Editor configuration.
+ClassicEditor.defaultConfig = {
+  toolbar: {
+    items: [
+      'bold',
+      'italic',
+      'underline',
+      '|',
+      'bulletedList',
+      'numberedList',
+      '|',
+      'link',
+    ],
+  },
+  // This value must be kept in sync with the language defined in webpack.config.js.
+  language: 'en',
 };
